708ff554d3a34c6f06d317bacf7de0707908332d
Joha <Joha@DESKTOP-ANU2LNI>
2024-04-16 22:52:11 +0400

    Add project files.

---
+++ G19/DatabaseImporter.cs
@@ -0,0 +1,27 @@
+﻿namespace G19_ProductImport
+{
+    public class DatabaseImporter
+    {
+        private readonly string _connectionString;
+
+        public DatabaseImporter(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public void ImportData(IEnumerable<Category> categories)
+        {
+            throw new NotImplementedException();
+        }
+        static void FileStream()
+        {
+            string ProductsFile = @"D:\products2.txt";
+            string[] lines = File.ReadAllLines(ProductsFile);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('\t');
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ G19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace G19_ProductImport
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const string filePath = @"D:\products2.txt";
+            const string connectionString = "Server=.;Database=ProductImport;Integrated Security=True;";
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            try
+            {
+                var categories = FileManager.ReadData(filePath);
+                foreach (var category in categories)
+                {
+                    Console.WriteLine(category);
+                    foreach(var product in category.Products)
+                    {
+                        Console.WriteLine($"\t{product}");
+                    }
+                }
+
+                //importer.ImportData(categories);
+                //var importer = new DatabaseImporter(connectionString);
+                //Console.WriteLine("Data imported.");
+                stopwatch.Stop();
+                Console.WriteLine($"{stopwatch.ElapsedMilliseconds} - Time");
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
